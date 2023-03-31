0bf82d306377045a0606a5d9ed7e0ff14ff5252d
JosieSavill <fiordasavilljosie@gmail.com>
2023-03-31 09:27:01 -0700

    added folders: helpers middleware; added files: fsUtils.js, clog.js, index.js, server.js

---
+++ Develop/server.js
@@ -0,0 +1,23 @@
+// const express = require('express');
+
+
+
+
+// const PORT = process.env.port || 3001;
+
+// const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+// app.listen(PORT, () =>
+//   console.log(`App listening at http://localhost:${PORT} 🚀`)
+// );
